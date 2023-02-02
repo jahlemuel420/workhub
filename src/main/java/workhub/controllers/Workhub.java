3c7c30e8d2dcc242ded9d2f1266922f5d161dc6e
Apollo Joaquin <Apollo Joaquin@DESKTOP-P3094QC>
2023-02-03 01:04:56 +0800

    Initial Commit

---
+++ src/main/java/workhub/controllers/Workhub.java
@@ -0,0 +1,13 @@
+package workhub.controllers;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ApplicationScoped;
+
+@ApplicationScoped
+public class Workhub implements Serializable 
+{
+	// serial
+	private static final long serialVersionUID = 1065958486375497164L;
+
+}
